2d93dd4175a5200c741a8ae9e20fce9e769cb428
Tr Thanh Long <you@example.com>
2021-04-19 22:10:59 +0700

    Add product to cart api

--- mern-back-end/src/index.server.js
+++ mern-back-end/src/index.server.js
@@ -8,6 +8,7 @@ const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin/auth');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
+const cartRoutes = require('./routes/cart');
 
 
 //environment variable or constants
@@ -31,6 +32,7 @@ app.use('/api',authRoutes);
 app.use('/api',adminRoutes);
 app.use('/api',categoryRoutes);
 app.use('/api',productRoutes);
+app.use('/api',cartRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
