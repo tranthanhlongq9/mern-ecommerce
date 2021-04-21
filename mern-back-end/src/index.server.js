3b4dfbceef58256771e07b229daeca5a8ece893b
Tr Thanh Long <you@example.com>
2021-04-21 12:31:00 +0700

    fix Category, Cart api

--- mern-back-end/src/index.server.js
+++ mern-back-end/src/index.server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const env = require('dotenv');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
 
 //routes
 const authRoutes = require('./routes/auth');
@@ -28,6 +29,7 @@ mongoose.connect(
 });
 
 app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, 'uploads')));
 app.use('/api',authRoutes);
 app.use('/api',adminRoutes);
 app.use('/api',categoryRoutes);
--- mern-back-end/src/routes/category.js
+++ mern-back-end/src/routes/category.js
@@ -3,9 +3,24 @@ const { requireSignin, adminMiddleware } = require('../common-middleware');
 
 const { addCategory, getCategories } = require('../controller/category');
 const router = express.Router();
+const shortid = require('shortid');
+const path = require('path');
+const multer = require('multer');
 
 
-router.post('/category/create',requireSignin ,adminMiddleware ,addCategory);
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join( path.dirname(__dirname), 'uploads' ))
+    },
+    filename: function (req, file, cb){
+        cb(null, shortid.generate() + '-' + file.originalname)
+    }
+});
+
+const upload = multer({ storage });
+
+
+router.post('/category/create', requireSignin, adminMiddleware, upload.single('categoryImage'), addCategory);
 router.get('/category/getcategory', getCategories);
 
 
